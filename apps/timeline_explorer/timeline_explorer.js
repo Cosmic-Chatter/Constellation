e5938a09ac74f95e215ce8306ea823d61bf5216b
Morgan Rehnberg <forceflow1049@gmail.com>
2023-01-30 15:57:02 -0600

    First dynamic entry building

--- apps/timeline_explorer/timeline_explorer.js
+++ apps/timeline_explorer/timeline_explorer.js
@@ -1,3 +1,5 @@
+/* global showdown */
+
 import * as constCommon from '../js/constellation_app_common.js'
 
 function updateFunc (update) {
@@ -11,7 +13,8 @@ function updateFunc (update) {
   // This should be last to make sure the path has been updated
   if ('content' in update) {
     if (update.content[0] !== currentContent) {
-
+      currentContent = update.content[0]
+      loadDefinition(currentContent)
     }
   }
 }
@@ -33,10 +36,55 @@ function _loadDefinition (def) {
   // Helper function to manage setting up the interface.
 
   $('#headerText').html(def.title || '')
+
+  // Load the CSV file containing the timeline data and use it to build the timeline entries.
+  constCommon.makeHelperRequest({
+    method: 'GET',
+    endpoint: '/content/Apollo_timeline.csv',
+    rawResponse: true
+  })
+    .then((response) => {
+      $('#timelineContainer').empty()
+      constCommon.csvToJSON(response).forEach((entry) => {
+        createTimelineEntry(entry)
+      })
+    })
 }
 
-// define variables
-const items = document.querySelectorAll('.timeline li')
+function createTimelineEntry (entry) {
+  // Take the provided object and turn it into a set of HTML elements representing the entry.
+
+  // Initialize the Markdown converter
+  const converter = new showdown.Converter({ parseImgDimensions: true })
+
+  const li = document.createElement('li')
+
+  const container = document.createElement('div')
+  container.classList = 'timeline-element'
+  li.appendChild(container)
+
+  const timeEl = document.createElement('time')
+  timeEl.innerHTML = converter.makeHtml(entry.Time)
+  container.appendChild(timeEl)
+
+  const title = document.createElement('div')
+  if (parseInt(entry.Level) < 1) {
+    title.classList = 'size1'
+  } else if (parseInt(entry.Level) > 4) {
+    title.classList = 'size4'
+  } else {
+    title.classList = 'size' + parseInt(entry.Level)
+  }
+  title.innerHTML = converter.makeHtml(entry.Title)
+  container.appendChild(title)
+
+  const bodyDiv = document.createElement('div')
+  bodyDiv.innerHTML = converter.makeHtml(entry.Body)
+  container.appendChild(bodyDiv)
+
+  $('#timelineContainer').append(li)
+  configureVisibleElements()
+}
 
 // check if an element is in viewport
 // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
@@ -51,7 +99,11 @@ function isElementInViewport (el) {
   )
 }
 
-function callbackFunc () {
+function configureVisibleElements () {
+  // Iterate the timeline elements and make them visible if they are in view (trigger the animation).
+
+  const items = document.querySelectorAll('.timeline li')
+
   for (let i = 0; i < items.length; i++) {
     if (isElementInViewport(items[i])) {
       items[i].classList.add('in-view')
@@ -62,12 +114,12 @@ function callbackFunc () {
 }
 
 // Add event listeners
-window.addEventListener('load', callbackFunc)
-window.addEventListener('resize', callbackFunc)
-document.getElementById('timeline-pane').addEventListener('scroll', callbackFunc)
+window.addEventListener('load', configureVisibleElements)
+window.addEventListener('resize', configureVisibleElements)
+document.getElementById('timeline-pane').addEventListener('scroll', configureVisibleElements)
 
 // Constellation stuff
-const currentContent = ''
+let currentContent = ''
 constCommon.config.updateParser = updateFunc // Function to read app-specific updatess
 constCommon.config.constellationAppID = 'timeline_explorer'
 constCommon.config.debug = true
