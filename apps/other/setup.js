5d6eb45671a4702a1d78c0dac65a0f958be698d5
Morgan Rehnberg <forceflow1049@gmail.com>
2023-08-26 15:34:49 -0500

    Completed other app setup.

--- apps/other/setup.js
+++ apps/other/setup.js
@@ -13,11 +13,13 @@ function clearDefinitionInput (full = true) {
       .then((response) => {
         $('#definitionSaveButton').data('initialDefinition', {
           uuid: response.uuid,
-          path: ''
+          path: '',
+          properties: {}
         })
         $('#definitionSaveButton').data('workingDefinition', {
           uuid: response.uuid,
-          path: ''
+          path: '',
+          properties: {}
         })
         previewDefinition(false)
       })
@@ -25,6 +27,8 @@ function clearDefinitionInput (full = true) {
 
   // Definition details
   document.getElementById('definitionNameInput').value = ''
+  document.getElementById('keyList').innerHTML = ''
+  document.getElementById('pathInput').value = ''
 }
 
 function createNewDefinition () {
@@ -42,6 +46,11 @@ function editDefinition (uuid = '') {
   $('#definitionSaveButton').data('workingDefinition', structuredClone(def))
 
   $('#definitionNameInput').val(def.name)
+
+  document.getElementById('pathInput').value = def.path
+  Object.keys(def.properties).forEach((key) => {
+    createKeyValueHTML(key, def.properties[key])
+  })
 
   // Configure the preview frame
   if (def.path !== '') {
@@ -112,17 +121,17 @@ function createKeyValueHTML (key = '', value = '') {
   const uuid = String(new Date().getTime() * Math.random() * 1e6)
 
   const html = `
-  <div id="keyValue_${uuid}" class="col-12">
+  <div id="keyValue_${uuid}" data-uuid="${uuid}" class="keyValuePair col-12">
     <div class="row align-items-center">
       <div class="col-12 col-md-5">
         <div class="form-floating mb-3">
-          <input id="key_${uuid}" type="text" class="form-control" placeholder="" value="${key}">
+          <input id="key_${uuid}" type="text" class="form-control keyValueInput" placeholder="" value="${key}">
           <label for="key_${uuid}">Key</label>
         </div>
       </div>
       <div class="col-12 col-md-5">
         <div class="form-floating mb-3">
-          <input id="value_${uuid}" type="text" class="form-control" placeholder="" value="${value}">
+          <input id="value_${uuid}" type="text" class="form-control keyValueInput" placeholder="" value="${value}">
           <label for="value_${uuid}">Value</label>
         </div>
       </div>
@@ -133,6 +142,21 @@ function createKeyValueHTML (key = '', value = '') {
   </div>
   `
   keyList.innerHTML += html
+  document.getElementById('key_' + uuid).addEventListener('change', rebuildPropertyDict)
+  document.getElementById('value_' + uuid).addEventListener('change', rebuildPropertyDict)
+}
+
+function rebuildPropertyDict () {
+  // Build a dictionary of all the keyValue pairs and add it to the definition.
+
+  const dict = {}
+  Array.from(document.querySelectorAll('.keyValuePair')).forEach((keyValue) => {
+    const uuid = keyValue.getAttribute('data-uuid')
+    const key = document.getElementById('key_' + uuid).value
+    if (key.trim() === '') return
+    dict[key] = document.getElementById('value_' + uuid).value
+  })
+  constSetup.updateWorkingDefinition(['properties'], dict)
 }
 
 // Set helperAddress for calls to constCommon.makeHelperRequest
@@ -156,10 +180,12 @@ document.getElementById('pathInput').addEventListener('change', (event) => {
 document.getElementById('addKeyButton').addEventListener('click', (event) => {
   createKeyValueHTML()
 })
+
 document.addEventListener('click', (event) => {
   if (event.target.classList.contains('keyValue-delete') === false) return
   const uuid = event.target.getAttribute('data-uuid')
   document.getElementById('keyValue_' + uuid).remove()
+  rebuildPropertyDict()
 })
 
 // Set color mode
