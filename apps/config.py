118cbf4c3fb5de9ccc95dfbfc4ed06842a9a6ff8
Morgan Rehnberg <forceflow1049@gmail.com>
2023-09-14 13:09:55 -0500

    Remove unnecessary webview import

--- apps/config.py
+++ apps/config.py
@@ -4,8 +4,6 @@ import datetime
 import threading
 from typing import Any, Union
 
-import webview
-
 # Path to the directory where the server is being launched from
 application_path: str = ""
 # Path to the directory the code is actually running from (different from APP_PATH when using Pyinstaller)
